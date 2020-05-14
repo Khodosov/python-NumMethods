674fbbd7479ddbc76fdb0ec34c94948a31f2423d
Khodosov <43482216+Khodosov@users.noreply.github.com>
2020-05-14 18:45:57 +0300

    выполнил, проверил и задебажил 3 подпункта

--- trashcan.py
+++ trashcan.py
@@ -12,6 +12,17 @@ size = 10
 accuracy = 0.0001
 
 
+def swap_columns2(your_list, pos1, pos2):
+    for item in your_list:
+        item[pos1], item[pos2] = item[pos2], item[pos1]
+
+
+def swap_rows2(input_array, row1, row2):
+    temp = np.copy(input_array[row1][:])
+    input_array[row1][:] = input_array[row2][:]
+    input_array[row2][:] = temp
+
+
 def decompose_PAQ_LU(matrix, n):
     swaps = 0
     matrix_c = deepcopy(matrix)
@@ -47,18 +58,8 @@ def decompose_PAQ_LU(matrix, n):
     return matrix_l, matrix_u, matrix_p, matrix_q, swaps
 
 
-def swap_columns2(your_list, pos1, pos2):
-    for item in your_list:
-        item[pos1], item[pos2] = item[pos2], item[pos1]
-
-
-def swap_rows2(input_array, row1, row2):
-    temp = np.copy(input_array[row1][:])
-    input_array[row1][:] = input_array[row2][:]
-    input_array[row2][:] = temp
-
-
 def rang(matrix, n):
+    n = int(n)
     rank = n
     for row in range(n):
         if zeros(matrix[n - 1 - row], n):
@@ -69,13 +70,15 @@ def rang(matrix, n):
 
 
 def zeros(row, n):
+    n = int(n)
     for i in range(n):
         if abs(row[i]) - 0.0000001 > 0:
             return False
     return True
 
 
-def system_solution_2(matrix_l, matrix_u, matrix_p, matrix_q, b, n):
+# Для совместной
+def system_solution_2(matrix_l, matrix_u, n, b, matrix_p, matrix_q):
     operations_number = 0
     b = list(np.array(matrix_p).dot(np.array(b).transpose()))
     y = []
@@ -99,10 +102,11 @@ def system_solution_2(matrix_l, matrix_u, matrix_p, matrix_q, b, n):
     return x, operations_number
 
 
-def solve_3(matrix_l, matrix_u, matrix_p, matrix_q, b, rank, n):
+# Для решения системы с вырожденной матрицей
+def solve_3(L, U, P, Q, b, rank, n):
     operations_number = 0
-    cU = np.copy(matrix_u)
-    g = np.linalg.inv(matrix_l).dot(matrix_p).dot(b)
+    cU = np.copy(U)
+    g = np.linalg.inv(L).dot(P).dot(b)
     operations_number = 4 * n * n
     y = list(np.zeros(n))
     for i in range(rank)[::-1]:
@@ -115,12 +119,14 @@ def solve_3(matrix_l, matrix_u, matrix_p, matrix_q, b, rank, n):
             cU[j, :] -= cU[i, :] * cU[j, i]
             operations_number += 2 + 2 * n
         y[i] = g[i]
-    x = np.array(matrix_q).dot(np.array(y))
+    x = np.array(Q).dot(np.array(y))
     operations_number += n * n
     return x, operations_number
 
 
-# ----------------------------------------------------------------------------------------------------------------------
+# Далее новый код =====================================================================================================
+
+
 def func(a):
     res = []
     x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = symbols('x1 x2 x3 x4 x5 x6 x7 x8 x9 x10')
@@ -150,31 +156,26 @@ def jacobian(a):
     res = np.zeros((10, 10))
     x1, x2, x3, x4, x5, x6, x7, x8, x9, x10 = symbols('x1 x2 x3 x4 x5 x6 x7 x8 x9 x10')
     xs = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
-    Fs = Matrix([cos(x2 * x1) - exp(-3 * x3) + x4 * x5 ** 2 - x6 - sinh(
-        2 * x8) * x9 + 2 * x10 + 2.000433974165385440,
+    Fs = Matrix([cos(x2 * x1) - exp(-3 * x3) + x4 * x5 ** 2 - x6 - sinh(2 * x8) * x9 + 2 * x10 + 2.000433974165385440,
                  sin(x2 * x1) + x3 * x9 * x7 - exp(-x10 + x6) + 3 * x5 ** 2 - x6 * (x8 + 1) + 10.886272036407019994,
                  x1 - x2 + x3 - x4 + x5 - x6 + x7 - x8 + x9 - x10 - 3.1361904761904761904,
-                 2 * cos(-x9 + x4) + x5 / (x3 + x1) - sin(x2 ** 2) + cos(
-                     x7 * x10) ** 2 - x8 - 0.1707472705022304757,
-                 sin(x5) + 2 * x8 * (x3 + x1) - exp(-x7 * (-x10 + x6)) + 2 * cos(x2) - 1.0 / (
-                         -x9 + x4) - 0.3685896273101277862,
+                 2 * cos(-x9 + x4) + x5 / (x3 + x1) - sin(x2 ** 2) + cos(x7 * x10) ** 2 - x8 - 0.1707472705022304757,
+                 sin(x5) + 2 * x8 * (x3 + x1) - exp(-x7 * (-x10 + x6)) + 2 * cos(x2) - 1.0 / (-x9 + x4) - 0.3685896273101277862,
                  exp(x1 - x4 - x9) + x5 ** 2 / x8 + cos(3 * x10 * x2) / 2 - x6 * x3 + 2.0491086016771875115,
                  x2 ** 3 * x7 - sin(x10 / x5 + x8) + (x1 - x6) * cos(x4) + x3 - 0.7380430076202798014,
-                 x5 * (x1 - 2 * x6) ** 2 - 2 * sin(-x9 + x3) + 0.15e1 * x4 - exp(
-                     x2 * x7 + x10) + 3.5668321989693809040,
+                 x5 * (x1 - 2 * x6) ** 2 - 2 * sin(-x9 + x3) + 0.15e1 * x4 - exp(x2 * x7 + x10) + 3.5668321989693809040,
                  7 / x6 + exp(x5 + x4) - 2 * x2 * x8 * x10 * x7 + 3 * x9 - 3 * x1 - 8.4394734508383257499,
                  x10 * x1 + x9 * x2 - x8 * x3 + sin(x4 + x5 + x6) * x7 - 0.78238095238095238096])
     for i in range(10):
         for j in range(10):
-            res[i, j] = float(diff(Fs[i], xs[j]).subs(
-                [(x1, a[0]), (x2, a[1]), (x3, a[2]), (x4, a[3]), (x5, a[4]), (x6, a[5]), (x7, a[6]), (x8, a[7]),
-                 (x9, a[8]), (x10, a[9])]))
+            res[i, j] = float(diff(Fs[i], xs[j]).subs([(x1, a[0]), (x2, a[1]), (x3, a[2]), (x4, a[3]), (x5, a[4]), (x6, a[5]), (x7, a[6]), (x8, a[7]), (x9, a[8]), (x10, a[9])]))
     return res
 
 
 def SLAE_solve(matrix, _b, n):
-    L, U, P, Q, swaps = decompose_PAQ_LU(list(matrix), n)
-    # swaps не нужны
+    n = int(n)
+    L, U, P, Q, swaps = decompose_PAQ_LU(matrix, n)
+    # swaps нам не нужны
     rank = rang(U, n)
     Ab = np.zeros((n, n + 1))
     x = []
@@ -184,7 +185,7 @@ def SLAE_solve(matrix, _b, n):
     Ab[:, n] = np.copy(_b)
     if np.linalg.matrix_rank(Ab) == rank:
         if rank == n:
-            x, count = system_solution_2(L, U, P, Q, _b, n)
+            x, count = system_solution_2(L, U, n, _b, P, Q)
         else:
             x, count = solve_3(L, U, P, Q, _b, rank, n)
     return x, count
@@ -201,7 +202,7 @@ def newton_method(f, j, x0, n, accuracy):
         apr_x, ops = SLAE_solve(j(x_cur), -f(x_cur), n)
         operation_num += ops
         norm = np.linalg.norm(apr_x, np.inf)
-        x_cur = apr_x + x_cur
+        x_cur = apr_x[:].transpose() + x_cur
     duration = time.time() - t
     return x_cur, iter_num, operation_num, duration
 
@@ -213,31 +214,31 @@ def modified_newton_method(f, j, x0, n, accuracy):
     t = time.time()
     norm = 1000
     matrix = j(x_cur)
-    L, U, P, Q, swaps = decompose_PAQ_LU(list(matrix), n)
-    # swaps не нужны
+    L, U, P, Q, swaps = decompose_PAQ_LU(matrix, n)
+    # swaps нам не нужны
     while norm > accuracy:
         _b = -f(x_cur)
         rank = rang(U, n)
         Ab = np.zeros((n, n + 1))
         apr_x = []
-        count = 0
+        counter = 0
         for i in range(n):
             Ab[:, i] = np.copy(matrix[:, i])
         Ab[:, n] = np.copy(_b)
         if np.linalg.matrix_rank(Ab) == rank:
             if rank == n:
-                apr_x, count = system_solution_2(L, U, P, Q, _b, n)
+                apr_x, counter = system_solution_2(L, U, n, _b,  P, Q)
             else:
-                apr_x, count = solve_3(L, U, P, Q, _b, rank, n)
-        operation_num += count
+                apr_x, counter = solve_3(L, U, P, Q, _b, rank, n)
+        operation_num += counter
         iter_num += 1
         norm = np.linalg.norm(apr_x, np.inf)
-        x_cur = apr_x + x_cur
+        x_cur = apr_x[:].transpose() + x_cur
     duration = time.time() - t
     return x_cur, iter_num, operation_num, duration
 
 
-def newton_method_with_change(f, j, x0, n, k, accuracy):
+def newton_method_with_change(f, j, x0, k, n, accuracy):
     x_cur = x0
     iter_num = 0
     operation_num = 0
@@ -246,28 +247,28 @@ def newton_method_with_change(f, j, x0, n, k, accuracy):
         iter_num += 1
         apr_x, ops = SLAE_solve(j(x_cur), -f(x_cur), n)
         operation_num += ops
-        x_cur = apr_x + x_cur
+        x_cur = apr_x[:,0].transpose() + x_cur
     x_cur, itn, opn, _ = modified_newton_method(f, j, x_cur, n, accuracy)
     duration = time.time() - t
     return x_cur, iter_num + itn, operation_num + opn, duration
 
 
-def newton_method_with_period(f, j, x0, period, n):
+def newton_method_with_period(f, j, x0, p, accuracy, n):
     x_cur = x0
     iter_num = 0
     operation_num = 0
     t = time.time()
     norm = 1000
     matrix = j(x_cur)
-    L, U, P, Q, swaps = decompose_PAQ_LU(list(matrix), n)
-    # swaps не нужны
+    L, U, P, Q, swaps = decompose_PAQ_LU(matrix, n)
+    # swaps нам не нужны
     period = 0
     while norm > accuracy:
         period += 1
-        if period == period:
+        if period == p:
             matrix = j(x_cur)
-            L, U, P, Q, swaps = decompose_PAQ_LU(list(matrix), n)
-            # swaps не нужны
+            L, U, P, Q, swaps = decompose_PAQ_LU(matrix, n)
+            # swaps нам не нужны
             period = 0
         iter_num += 1
         _b = -f(x_cur)
@@ -280,35 +281,11 @@ def newton_method_with_period(f, j, x0, period, n):
         Ab[:, n] = np.copy(_b)
         if np.linalg.matrix_rank(Ab) == rank:
             if rank == n:
-                apr_x, count = system_solution_2(L, U, P, Q, _b, n)
+                apr_x, count = system_solution_2(L, U, n, _b, P, Q)
             else:
                 apr_x, count = solve_3(L, U, P, Q, _b, rank, n)
         operation_num += count
         norm = np.linalg.norm(apr_x, np.inf)
-        x_cur = apr_x + x_cur
+        x_cur = apr_x[:,0].transpose() + x_cur
     duration = time.time() - t
     return x_cur, iter_num, operation_num, duration
-
-
-x_0 = np.array([0.5, 0.5, 1.5, -1, -0.5, 1.5, 0.5, -0.5, 1.5, -1.5])
-print("==== X_o =========================================================================================")
-print(str(x_0))
-x, i, u, d = newton_method(func, jacobian, x_0, size, accuracy)
-print("==== Метод Ньюьона ===============================================================================")
-print("==== X ===========================================================================================" + "\n", x)
-print("==== Количество операций =========================================================================" + "\n", u)
-print("==== Количество итераций =========================================================================" + "\n", i)
-print("==== Время =======================================================================================" + "\n", d)
-x, i, u, d = modified_newton_method(func, jacobian, x_0, size, accuracy)
-print("==== Модифицированный Метод Ньюьона ==============================================================")
-print("==== X ===========================================================================================" + "\n", x)
-print("==== Количество операций =========================================================================" + "\n", u)
-print("==== Количество итераций =========================================================================" + "\n", i)
-print("==== Время =======================================================================================" + "\n", d)
-k = 2
-x, i, u, d = newton_method_with_change(func, jacobian, x_0, size, k, accuracy)
-print("==== Модифицированный Метод Ньюьона ==============================================================")
-print("==== X ===========================================================================================" + "\n", x)
-print("==== Количество операций =========================================================================" + "\n", u)
-print("==== Количество итераций =========================================================================" + "\n", i)
-print("==== Время =======================================================================================" + "\n", d)
